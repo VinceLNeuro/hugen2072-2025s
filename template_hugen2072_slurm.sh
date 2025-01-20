08f80f06a15552ad0219cafb4acc56f3559d5071
til177 <til177@teach-cpu-n0.crc.pitt.edu>
2025-01-20 15:31:30 -0500

    Add a SLURM template for hugen2072

---
+++ template_hugen2072_slurm.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+######## Slurm resource allocation HUGEN2072 ########
+#SBATCH --job-name=CHANGE_THIS
+#SBATCH --cluster=teach
+#SBATCH --account=hugen2072-2025s
+#SBATCH --time=1:00:00 #1hr
+
+#SBATCH --nodes=1 #default - all cores on one machine
+#SBATCH --ntasks-per-node=1 #default
+#SBATCH --cpus-per-task=2 
+#SBATCH --mem=2000MB #2GB
+#SBATCH --mail-user=til177@pitt.edu
+#SBATCH --mail-type=END,FAIL
+
+#SBATCH --output=../RELATIVE/PATH/%x-slurm_%A.out
+
+######## Load software into environment ########
+# module purge
+# module load plink/1.90_20230116 #gcc/8.2.0 bcftools/1.15.1
+
+set -e
+# set -euo pipefail # detect errors
+set -v            # print all commands
+
+echo "Job started at: $(date)"
+
+
+#### Main analysis ####
+
+
+
+#### Main analysis end ####
+
+echo "Job ended at: $(date) on node $HOSTNAME"
+
+crc-job-stats
